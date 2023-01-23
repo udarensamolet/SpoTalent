f8222262003522a66b58c28a60fa8c84bfec2c51
Rositsa Nenova <udarensamolet@yahoo.com>
2023-01-23 17:30:25 +0200

    added v1

---
+++ SpoTalent/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpoTalent.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SpoTalent/Controllers/RecruitmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpoTalent.Controllers
+{
+    public class RecruitmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
